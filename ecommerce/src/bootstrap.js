093e5e459cc8485d4e949c569097765ad0faf52c
AndrewMPFQ <andrewf9001@gmail.com>
2019-07-11 13:56:07 -0600

    bootstrap and layout updated

--- ecommerce/src/bootstrap.js
+++ ecommerce/src/bootstrap.js
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
 
+import Layout from "./components/layout";
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Layout />
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
